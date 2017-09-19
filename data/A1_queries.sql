18e729488c739c65582d0544dd298c9cbc2bb5a1
Suzanne Little <suzanne.little.au@gmail.com>
2017-09-19 09:13:43 +0100

    data updates

---
+++ data/A1_queries.sql
@@ -0,0 +1,13 @@
+-- Q1: What is the best carpark for The Helix?
+
+
+-- Q2: How many disabled carpark spaces are there on each campus?
+
+
+-- Q3: Historically, how occupied is St. Pats carpark in week 6 between 10am and 11am?
+
+
+-- Q4: Where can a member of the general public park at 5:30pm on 18th of September 2016? Answer should provide both a campus name and a car park name.
+
+
+-- Q5: When is the best time to arrive at DCU Glasnevin to be able to park near Invent?
