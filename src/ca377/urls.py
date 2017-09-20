efbdd446127f9906ccb2461bdb0a47854f0f78c2
Declan Moore <declan.moore39@mail.dcu.ie>
2017-09-20 14:29:51 +0100

    test commit

--- src/ca377/urls.py
+++ src/ca377/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import patterns, url, include
+from django.conf.urls import *
 from django.contrib import admin
 
 # register the urls for the park_at_dcu application (HINT: use the include function)
