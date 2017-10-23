99318c06b194152cf067c093aa67aed335377d62
Declan Moore <declan.moore39@mail.dcu.ie-ca377-moored39-parkatdcu>
2017-10-23 18:40:41 +0100

    A2 attempt

--- src/park_at_dcu/views.py
+++ src/park_at_dcu/views.py
@@ -4,31 +4,30 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+import urllib
 import json
 import requests
 
-def index(requests):
+# Create your index view here. It should display the message 'Welcome to Park@DCU!'
 
-   #return HttpResponse("Welcome to Park@DCU!")
-
+def index(request):
    template = loader.get_template('park_at_dcu/index.html')
-   return HttpResponse(template.render({},requests))
+   return HttpResponse(template.render({},request))
 
-def webservice(requests):
+def webservice(request):
     template = loader.get_template('park_at_dcu/webservice.html')
-    return HttpResponse(template.render({},requests))
+    carpark= request.GET['carpark']
+    url= "http://suzannelittle.pythonanywhere.com/carpark/" + carpark
+    output=requests.get(url)
+    parsed_json = json.loads(output.text)
+    parsed_json = {'parsed_json': parsed_json}
 
-    # write code to:
+    return HttpResponse(template.render(parsed_json,request))
+
+
+
+# write code to:
     # 1) call the webservice with a particular car park
     # 2) retrieve the returned json in the form of a Python dictionary
     # 3) handle errors in the json (see the 'bad' version of the webservice)
     # 4) render the template (see last line in index method)
-    
-#r = requests.get("http://suzannelittle.pythonanywhere.com/carpark/StPats")
-#r.text
-
-
-#data = json.loads(r.text)
-
-#for item in data:
-  #print data[item]
