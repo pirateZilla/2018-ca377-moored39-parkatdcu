3bd4c8c1dcb552e99e9179056248b8a35fb09795
Jennifer Foster <jenniferfoster@Jennifers-MacBook-Air.local>
2017-10-24 10:50:10 +0100

    A3 updates

--- src/park_at_dcu/tests.py
+++ src/park_at_dcu/tests.py
@@ -4,6 +4,9 @@ from __future__ import unicode_literals
 from django.test import TestCase
 from django.urls import reverse
 
+from park_at_dcu.models import Carpark,Facility,Campus,HistoricalData
+from datetime import time
+
 # Create your tests here.
 class IndexTests(TestCase):
 
@@ -37,7 +40,8 @@ class WebServiceTests(TestCase):
         self.assertContains(response,'An error has occurred')
         self.assertNotContains(response, 'Spaces Available')
         self.assertNotContains(response,'<table>')
-            
+        
+    
     def webservice(self, carpark):
         """                                                                            Test calling webservice for individual carpark  
         """
@@ -53,3 +57,35 @@ class WebServiceTests(TestCase):
         self.assertNotContains(response, 'Spaces Available')
         self.assertNotContains(response,'<table>')
          
+class DatabaseTests(TestCase):
+   
+    def setup(self):
+        """
+        Sets up a test database
+        """
+        campus = Campus(1,'Test Campus')
+        carpark1 = Carpark(1, 'Test CP1', 1,'off Collins Ave',time(hour=8),time(hour=22),750,17,"53.386284, -6.256948",0,1)
+        carpark2 = Carpark(2, 'Test CP2', 1,'Ballymun Road',time(hour=7),time(hour=22),250,23,"53.386284, -6.256948",0,1)
+        facility = Facility(1,'Test Facility',1)
+        campus.save()
+        carpark1.save()
+        carpark2.save()
+        facility.save()
+        
+    def test_facility(self):
+        """                                                                            Test retrieving carpark spaces
+        """
+        self.setup()
+        response = self.client.get(reverse('park_at_dcu:facility'),{'facility':'Test Facility'})
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response, 'CP1')
+
+    def test_spaces(self):
+        """                                                                            Test retrieving carpark spaces
+        """
+        self.setup()
+        response = self.client.get(reverse('park_at_dcu:spaces'),{'campus':'Test Campus'})
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response, '1000')
+
+        
--- src/park_at_dcu/views.py
+++ src/park_at_dcu/views.py
@@ -7,15 +7,89 @@ from django.template import loader
 import json
 import requests
 
+from .models import Carpark, Facility, Campus
+
 def index(request):
    template = loader.get_template('park_at_dcu/index.html')
    return HttpResponse(template.render({},request))
 
 def webservice(request):
     template = loader.get_template('park_at_dcu/webservice.html')
-    # write code to:
-    # 1) call the webservice with a particular car park
-    # 2) retrieve the returned json in the form of a Python dictionary
-    # 3) handle errors in the json (see the 'bad' version of the webservice)
-    # 4) render the template (see last line in index method)
+    carpark = request.GET.get('carpark', '')
+    version = request.GET.get('version', '')
+    webservice_url = 'http://suzannelittle.pythonanywhere.com/carpark/'+carpark
     
+    if version == 'bad':
+       real_time_info = requests.get(webservice_url+'?version=bad')
+    else:
+       real_time_info = requests.get(webservice_url)
+         
+    error_msg = get_error_msg(real_time_info)
+    if error_msg == '':
+       return HttpResponse(template.render(real_time_info.json(),request))
+    else:
+       return HttpResponse(template.render({'error_msg':error_msg},request))
+       
+def get_error_msg(info):
+    '''
+    returns an error message if there is a problem with the data 
+    returned by the webservice
+    '''
+    try:
+       info_json = info.json()
+       if 'error_msg' in info_json:
+         return info_json['error_msg']
+       elif 'timestamp' not in info_json:
+         return 'Invalid key'
+       elif 'spaces_available' not in info_json:
+         return 'Invalid key' 
+       elif 'carpark_name' not in info_json:
+         return 'Invalid key'
+       elif info_json['spaces_available'] < 0:
+         return 'Invalid value'
+       elif isinstance(info_json['spaces_available'],float):
+         return 'Invalid value'
+       return ''
+    except:
+         return 'Invalid JSON'   
+
+
+def facility(request):
+    '''
+    display the car parks suitable for a particular facility
+    '''
+    template = loader.get_template('park_at_dcu/facility.html')
+    try:
+       facility = request.GET.get('facility')
+       carparks = Carpark.objects.filter(facility__facility_name=facility)
+       if len(carparks) == 0:
+         return HttpResponse(template.render({'error_msg':'No carparks for ' + facility}))
+    except Facility.DoesNotExist:
+       return HttpResponse(template.render({'error_msg':'Facility does not exist'}))
+    except Carpark.DoesNotExist:
+       return HttpResponse(template.render({'error_msg':'Carpark does not exist'}))
+    return HttpResponse(template.render({'carparks':carparks},request))
+
+
+def spaces(request):
+    '''
+    display the number of spaces in a campus
+    '''
+    template = loader.get_template('park_at_dcu/spaces.html')
+    # write code for Q2
+
+def occupancy(request):
+    '''
+    display the historical occupancy for a particular carpark at week 10, 3pm
+    '''
+    template = loader.get_template('park_at_dcu/occupancy.html')
+    # write code for Q3
+
+def carpark_for_time(request):
+    '''
+    display the carparks for a particular time on 26th Sept. 2016
+    '''
+    template = loader.get_template('park_at_dcu/carpark_for_time.html')
+    # write code for Q4
+
+
