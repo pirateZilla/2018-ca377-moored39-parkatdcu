8e470e5c0ade5e36a31707a233765965c7264d8f
Jennifer Foster <jenniferfoster@Jennifers-MacBook-Air.local>
2017-10-30 11:55:27 +0000

    Registering the facility view

--- src/park_at_dcu/urls.py
+++ src/park_at_dcu/urls.py
@@ -7,5 +7,6 @@ app_name = 'park_at_dcu'
 # register the index view
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^webservice/$', views.webservice, name='webservice')
+    url(r'^webservice/$', views.webservice, name='webservice'),
+    url(r'^facility/$', views.facility, name='facility'),
     ]
