5042670c0315aa0cf2c1851784401f6082d80db6
Jennifer Foster <jenniferfoster@Jennifers-MacBook-Air.local>
2017-11-10 12:28:02 +0000

    Registered all views

--- src/park_at_dcu/urls.py
+++ src/park_at_dcu/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^webservice/$', views.webservice, name='webservice'),
     url(r'^facility/$', views.facility, name='facility'),
+    url(r'^spaces/$', views.spaces, name='spaces'),
+    url(r'^occupancy/$', views.occupancy, name='occupancy'),
+    url(r'^carpark_for_time/$', views.carpark_for_time, name='carpark_for_time'),
     ]
